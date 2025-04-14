c5d155ccc18ac34376f5b78261313296c26aad28
vitor <vitor_oliveira_20@hotmail.com>
2025-04-14 22:01:45 +0100

    update footer

--- 05-restaurant/include/footer.php
+++ 05-restaurant/include/footer.php
@@ -1,5 +1,5 @@
                             <!-- Footer -->
 <footer>
-    <p>Culinary Cove's website.</p>
-    <p>For educational purposesS</p>
+    <p><i class="fa-solid fa-mortar-pestle"></i> Culinary Cove's website.</p>
+    <p><i class="fa-solid fa-user-graduate"></i> For educational purposes</p>
 </footer>
